f9ff454166733cd3c0242d065d53aa762d552d5e
Guilherme Gades <tc.guilhermevargas@gruporbs.com.br>
2018-07-10 13:48:44 -0300

    :sparkles: Smooth scroll

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,48 @@ class PressStart {
 	constructor() {
 		this.highlightArrow();
 		this.scaleTitle();
+		this.bindEvents();
 	}
-	
+
 	scaleTitle() {
-		const title = document.querySelector("h1");
+		const title = document.querySelector('h1');
 		setTimeout(() => {
 			title.classList += 'ready';
 		}, 1000);
 	}
-	
+
 	highlightArrow() {
-		const arrow = document.querySelector(".title-arrow");
+		const arrow = document.querySelector('.title-arrow');
 		let show = true;
-		
+
 		setInterval(() => {
 			if (show) {
-				arrow.style.display = "none";
+				arrow.style.display = 'none';
 				show = false;
 			} else {
-				arrow.style.display = "inline";
+				arrow.style.display = 'inline';
 				show = true;
 			}
-				
+
 		}, 500);
+	}
+
+	bindEvents() {
+		const links = document.querySelectorAll('.scroll-to');
+		const self = this;
+		for (let i=0; i < links.length; i++) {
+			links[i].addEventListener('click', this.scrollTo, false);
+		}
+	}
+
+	scrollTo(e) {
+		e.preventDefault();
+		const target = e.target;
+		const id = target.getAttribute('href');
+		const section = document.querySelector(id);
+		const top = section.offsetTop;
+		console.log(top);
+		window.scroll({ top: top - 60, left: 0, behavior: 'smooth' });
 	}
 }
 
